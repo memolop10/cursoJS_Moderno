ce4887b15459ee0c2ab6427bb269fc5b2a1dd62b
memolop10 <inglopezvargas@gmail.com>
2021-04-20 20:34:46 -0500

    se agrego mensaje de Error y validaciones

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -16,5 +16,30 @@ function eventListeners() {
  function agregarTweet(e){
      e.preventDefault()
 
-     console.log('agregar Tweet');
+    //Text area donde el usuario escribe 
+     const tweet = document.querySelector('#tweet').value;
+     
+     //validacion
+     if (tweet === '') {
+         mostrarError('no puede ir vacio')
+         return;// evita que se ejecuten mas lineas de codigo
+     }
+
+     console.log('Agregando tweet')
+ }
+
+ //Mostrar mensaje de error
+ function mostrarError(error) {
+     const mensajeError = document.createElement('p');
+     mensajeError.textContent = error;
+     mensajeError.classList.add('error');
+
+     //Insertar en contenido
+     const contenido = document.querySelector('#contenido');
+     contenido.appendChild(mensajeError);
+
+     //Elimina el mensaje de Error despues de 4 segundos
+     setTimeout(() => {
+        mensajeError.remove();
+     },4000);
  }
